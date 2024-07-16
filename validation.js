759ee82137ed5f27815a98b7b146f4c1c43da9d6
Rin Kimhun <47348954+kimkimhun3@users.noreply.github.com>
2024-07-16 18:32:33 +0900

    Update validation.js

--- validation.js
+++ validation.js
@@ -61,15 +61,15 @@
   });
   
   // Buffer 2 validation
-  document.getElementById('buffer2-times').addEventListener('blur', function() {
-    const timesInput = this.value.trim();
-    const timesError = document.getElementById('buffer2-times-error');
-    if (timesInput === '' || timesInput < 100 || timesInput > 2000) {
-        timesError.textContent = 'Value must be between 100 and 2000';
-    } else {
-        timesError.textContent = '';
-    }
-  });
+//   document.getElementById('buffer2-times').addEventListener('blur', function() {
+//     const timesInput = this.value.trim();
+//     const timesError = document.getElementById('buffer2-times-error');
+//     if (timesInput === '' || timesInput < 100 || timesInput > 2000) {
+//         //timesError.textContent = 'Value must be between 100 and 2000';
+//     } else {
+//         //timesError.textContent = '';
+//     }
+//   });
   
   
   document.getElementById('packet-loss-switch').addEventListener('change', function() {
@@ -216,6 +216,8 @@ document.getElementById('buffer1-check').addEventListener('change', function() {
     }
 });
 
+// const buffer2TimesInput = document.getElementById('buffer2-times');
+
 
 
 // Buffer 2 checkbox change event listener
@@ -224,6 +226,7 @@ document.getElementById('buffer2-check').addEventListener('change', function() {
     const buffer2RepetitionInput = document.getElementById('buffer2-repetition');
     const buffer2TimesError = document.getElementById('buffer2-times-error');
     const buffer2RepetitionError = document.getElementById('buffer2-repetition-error');
+    const bufferTime = parseInt(document.getElementById('buffer2-times').value, 10);
     let isInteractedTimes = false; // Track if user has interacted with times input
     let isInteractedRepetition = false; // Track if user has interacted with repetition input
 
@@ -234,19 +237,33 @@ document.getElementById('buffer2-check').addEventListener('change', function() {
 
         // Enable repetition buffer 2 switch and input if checked
         document.getElementById('buffer2-repetition-switch').disabled = false;
+        document.getElementById('buffer2-repetition').disabled = false;
+        //document.getElementById('buffer2-execute').disabled = false;
+        document.getElementById('buffer2-main-execute').disabled = false;
+        //document.getElementById('buffer2-execute-minus').disabled = false;
+        // if (bufferTime > 0){
+        //     document.getElementById('buffer2-execute-minus').disabled = false;
+        // }else{
+        //     document.getElementById('buffer2-execute-minus').disabled = true;
+        // }
+        
+        //document.getElementById('buffer2-execute-minus').disabled = false;
 
     } else {
         // Disable Buffer 2 inputs
-        document.getElementById('buffer2-execute').disabled = true;
+        //document.getElementById('buffer2-execute').disabled = true;
+        document.getElementById('buffer2-repetition').disabled = true;
+        document.getElementById('buffer2-main-execute').disabled = true;
+        //document.getElementById('buffer2-execute-minus').disabled = true;
         buffer2TimesInput.disabled = true;
         buffer2TimesInput.removeAttribute('required'); // Remove required attribute
-        buffer2TimesInput.value = ''; // Clear input value
-        buffer2TimesError.textContent = ''; // Clear error message
+        //buffer2TimesInput.value = ''; // Clear input value
+       // buffer2TimesError.textContent = ''; // Clear error message
 
         // Disable repetition buffer 2 switch and input
         document.getElementById('buffer2-repetition-switch').disabled = true;
-        document.getElementById('buffer2-repetition').disabled = true;
-        buffer2RepetitionError.textContent = ''; // Clear error message
+        //document.getElementById('buffer2-repetition').disabled = true;
+        //buffer2RepetitionError.textContent = ''; // Clear error message
 
         // Reset interaction flags
         isInteractedTimes = false;
@@ -256,59 +273,61 @@ document.getElementById('buffer2-check').addEventListener('change', function() {
     // Event listener for times input change
     buffer2TimesInput.addEventListener('input', function() {
         isInteractedTimes = true; // User has interacted with times input
-        validateBuffer2Times(); // Validate times input on input change
-        enableDisableAddition2Button(); // Check and enable/disable Addition2 button
+        //validateBuffer2Times(); // Validate times input on input change
+        //enableDisableAddition2Button(); // Check and enable/disable Addition2 button
     });
 
     // Event listener for repetition input change
     document.getElementById('buffer2-repetition').addEventListener('input', function() {
         isInteractedRepetition = true; // User has interacted with repetition input
         validateBuffer2Repetition(); // Validate repetition input on input change
-        enableDisableAddition2Button(); // Check and enable/disable Addition2 button
+        //enableDisableAddition2Button(); // Check and enable/disable Addition2 button
     });
 
     // Event listener for repetition switch change
     document.getElementById('buffer2-repetition-switch').addEventListener('change', function() {
         if (this.checked) {
             document.getElementById('buffer2-repetition').disabled = false;
+            document.getElementById('buffer2-repetition-switch-text').value = "ON";
             validateBuffer2Repetition(); // Validate repetition input on switch on
-            enableDisableAddition2Button(); // Check and enable/disable Addition2 button
+            //enableDisableAddition2Button(); // Check and enable/disable Addition2 button
         } else {
             document.getElementById('buffer2-repetition').disabled = true;
-            buffer2RepetitionError.textContent = ''; // Clear error message
-            enableDisableAddition2Button(); // Check and enable/disable Addition2 button
+            document.getElementById('buffer2-repetition-switch-text').value = "OFF";
+            //buffer2RepetitionError.textContent = ''; // Clear error message
+            //enableDisableAddition2Button(); // Check and enable/disable Addition2 button
         }
     });
 
     // Validate Buffer 2 times input function
-    function validateBuffer2Times() {
-        const timesValue = buffer2TimesInput.value;
-        if (isInteractedTimes && (timesValue === '' || timesValue > 2000)) {
-            buffer2TimesError.textContent = 'Value must be between 100 and 2000';
-            return false;
-        } else {
-            buffer2TimesError.textContent = '';
-            return true;
-        }
-    }
+    // function validateBuffer2Times() {
+    //     const timesValue = buffer2TimesInput.value;
+    //     if (isInteractedTimes && (timesValue === '' || timesValue > 2000)) {
+    //         buffer2TimesError.textContent = 'Value must be between 100 and 2000';
+    //         return false;
+    //     } else {
+    //         //buffer2TimesError.textContent = '';
+    //         return true;
+    //     }
+    // }
 
     // Validate Buffer 2 repetition input function
     function validateBuffer2Repetition() {
         const repetitionValue = document.getElementById('buffer2-repetition').value;
         if (isInteractedRepetition && (repetitionValue === '' || repetitionValue <= 0 || repetitionValue > 30)) {
-            buffer2RepetitionError.textContent = 'Value must be between 1 and 30';
+            //buffer2RepetitionError.textContent = 'Value must be between 1 and 30';
             return false;
         } else {
-            buffer2RepetitionError.textContent = '';
+            //buffer2RepetitionError.textContent = '';
             return true;
         }
     }
 
     // Enable/disable Addition2 button based on validation
-    function enableDisableAddition2Button() {
-        const isValid = validateBuffer2Times() && (isInteractedRepetition ? validateBuffer2Repetition() : true);
-        document.getElementById('buffer2-execute').disabled = !isValid;
-    }
+    // function enableDisableAddition2Button() {
+    //     const isValid = validateBuffer2Times() && (isInteractedRepetition ? validateBuffer2Repetition() : true);
+    //     //document.getElementById('buffer2-execute').disabled = !isValid;
+    // }
 });
 
 
